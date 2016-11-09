afcd722421d108e3b36138e73a06a97a940c8fee
Joseph <jcdaniel55@gmail.com>
2016-11-09 13:50:58 -0500

    second commit

--- student-exercises/select-exercises.sql
+++ student-exercises/select-exercises.sql
@@ -5,3 +5,11 @@ SELECT ID, LASTNAME FROM Individual WHERE DeceasedDate IS NOT NULL;
 SELECT ID, LastName, BirthDate FROM Individual WHERE Year(BirthDate) > 1940;
 
 SELECT * FROM Individual WHERE ID IN (1,3,5,7,19);
+
+select * from Band where genre='alternative' and IsTogether=0;
+
+select * from Band where name like '% and %';
+
+select * from Individual where firstname like '_im';
+
+select * from Band where name like '%s';
