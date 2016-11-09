ca2d1657b8896cd7f241ace7e515085eb8a716d3
Joseph <jcdaniel55@gmail.com>
2016-11-09 11:57:25 -0500

    first commit

---
+++ student-exercises/select-exercises.sql
@@ -0,0 +1,7 @@
+select * from Individual where LastName = 'Jennings';
+
+SELECT ID, LASTNAME FROM Individual WHERE DeceasedDate IS NOT NULL;
+
+SELECT ID, LastName, BirthDate FROM Individual WHERE Year(BirthDate) > 1940;
+
+SELECT * FROM Individual WHERE ID IN (1,3,5,7,19);
